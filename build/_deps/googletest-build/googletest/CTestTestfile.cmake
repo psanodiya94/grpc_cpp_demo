25e09648dfc529643a216c20ba46e407b84d52d6
Pradumn Sanodiya <pradumnsanodiya2@gmail.com>
2025-10-01 00:38:51 +0530

    chore: add .gitignore and stop tracking build artifacts

--- build/_deps/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/psanodiya/workspace/cpp/grpc_cpp_demo/build/_deps/googletest-src/googletest
-# Build directory: /media/psanodiya/workspace/cpp/grpc_cpp_demo/build/_deps/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/psanodiya/workspace/cpp/grpc_cpp_demo/tests
-# Build directory: /media/psanodiya/workspace/cpp/grpc_cpp_demo/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/media/psanodiya/workspace/cpp/grpc_cpp_demo/build/tests/unit_tests[1]_include.cmake")
-subdirs("../_deps/googletest-build")
